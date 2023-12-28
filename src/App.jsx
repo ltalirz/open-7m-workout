5c4f06dacccd00c2d182d4a63c3b68e17926a08b
Leopold Talirz <leopold.talirz@gmail.com>
2023-12-28 12:33:55 +0100

    remove vite logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Workout from './components/Workout'
 
@@ -7,9 +6,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
