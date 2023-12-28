0655efb39755b7be9a8b543831613e2a50c6fccd
Leopold Talirz <leopold.talirz@gmail.com>
2023-12-28 11:25:59 +0100

    cleanup

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Timer from './Timer';
 import exercises from '../data/exercises.json'; // Import the JSON data
 
