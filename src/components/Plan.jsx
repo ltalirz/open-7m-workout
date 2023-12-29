5f9506ac5590963d42e956801176000ac99d5827
Leopold Talirz <leopold.talirz@gmail.com>
2023-12-29 10:04:09 +0100

    move out plan to separate file

---
+++ src/components/Plan.jsx
@@ -0,0 +1,44 @@
+import exercises from '../data/exercises.json'; // Import the JSON data
+
+const nSets = 3;
+const nCategories = Object.keys(exercises).length;
+const nExercisesPerCategory = 3;
+
+const generateWorkoutPlan = () => {
+    const totalBody = [...exercises.TotalBody];
+    const lowerBody = [...exercises.LowerBody];
+    const upperBody = [...exercises.UpperBody];
+    const core = [...exercises.Core];
+
+    let picks = [
+        pickRandom(totalBody, nExercisesPerCategory),
+        pickRandom(lowerBody, nExercisesPerCategory),
+        pickRandom(upperBody, nExercisesPerCategory),
+        pickRandom(core, nExercisesPerCategory)
+    ];
+    
+    let set = [];
+    for (let i = 0; i < nExercisesPerCategory; i++) {
+      for (let j = 0; j < nCategories; j++) {
+        set.push(picks[j][i]);
+      }
+    }
+    
+    let plan = new Array(nSets*set.length).fill(set).flat();
+
+    console.log(plan);
+    return plan;
+};
+
+const pickRandom = (array, count) => {
+    // Randomly pick 'count' elements from 'array', no repeats
+    let result = [];
+    while (result.length < count) {
+      let index = Math.floor(Math.random() * array.length);
+      result.push(array[index]);
+      array.splice(index, 1); // Remove selected exercise
+    }
+    return result;
+};
+
+export default generateWorkoutPlan;
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,11 +1,8 @@
 import { useState, useEffect } from 'react';
 import Timer from './Timer';
-import exercises from '../data/exercises.json'; // Import the JSON data
+import generateWorkoutPlan from './Plan';
 
 const Workout = () => {
-  const nSets = 3;
-  const nCategories = Object.keys(exercises).length;
-  const nExercisesPerCategory = 3;
   const exerciseDuration = 30;
   const restDuration = 5;
 
@@ -58,42 +55,6 @@ const Workout = () => {
     }
   };
 
-  const generateWorkoutPlan = () => {
-    const totalBody = [...exercises.TotalBody];
-    const lowerBody = [...exercises.LowerBody];
-    const upperBody = [...exercises.UpperBody];
-    const core = [...exercises.Core];
-
-    let picks = [
-        pickRandom(totalBody, nExercisesPerCategory),
-        pickRandom(lowerBody, nExercisesPerCategory),
-        pickRandom(upperBody, nExercisesPerCategory),
-        pickRandom(core, nExercisesPerCategory)
-    ];
-    
-    let set = [];
-    for (let i = 0; i < nExercisesPerCategory; i++) {
-      for (let j = 0; j < nCategories; j++) {
-        set.push(picks[j][i]);
-      }
-    }
-    
-    let plan = new Array(nSets*set.length).fill(set).flat();
-
-    console.log(plan);
-    return plan;
-  };
-
-  const pickRandom = (array, count) => {
-    // Randomly pick 'count' elements from 'array', no repeats
-    let result = [];
-    while (result.length < count) {
-      let index = Math.floor(Math.random() * array.length);
-      result.push(array[index]);
-      array.splice(index, 1); // Remove selected exercise
-    }
-    return result;
-  };
 
   const handleTimerComplete = () => {
     const nextIndex = workoutIndex + 1;
